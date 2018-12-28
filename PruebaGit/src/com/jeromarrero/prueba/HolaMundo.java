b99f4b13fecc546a3a95d31991f5fa4e4302b89f
cyrusgc <cyrusgc@gmail.com>
2018-12-28 11:18:08 +0100

    Update HolaMundo.java

--- PruebaGit/src/com/jeromarrero/prueba/HolaMundo.java
+++ PruebaGit/src/com/jeromarrero/prueba/HolaMundo.java
@@ -3,7 +3,7 @@ package com.jeromarrero.prueba;
    public class HolaMundo {
      
     	public static void main(String[] args) {		
-    		System.out.println("¡Hola Mundo!");
+    		System.out.println("Â¡Hola Mundoooo!");
     	}
      
     }
